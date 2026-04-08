36afc714567232ca4f7a4c61995fd85b4df9f8e6
ahmet <turksab.yonetim@gmail.com>
2026-04-08 17:46:16 +0300

    - refactor: learning center temizlendi, cookie consent varsayılanları ve vite polling yapılandırıldı

--- src/pages/cookies.ts
+++ src/pages/cookies.ts
@@ -3,25 +3,37 @@
  */
 import '../style.css'
 import { initFlowbite } from 'flowbite'
+import { TopBar, SubHeader, MegaMenu, initMegaMenu, initStickyHeaderSearch, initMobileDrawer } from '../components/header'
+import { initLanguageSelector } from '../components/header/TopBar'
+import { FooterLinks } from '../components/footer'
 import { FloatingPanel } from '../components/floating'
 import { startAlpine } from '../alpine'
-import { HelpCenterHeader, initHelpCenterLangSelector } from '../components/help-center'
 import { LegalPageLayout, CookieConsentUI } from '../components/legal'
 import { cookiesContent } from '../data/legalContent'
 
 const appEl = document.querySelector<HTMLDivElement>('#app')!;
 appEl.classList.add('relative');
 appEl.innerHTML = `
-  ${HelpCenterHeader({ activePage: 'cookies' })}
+  <div id="sticky-header" class="sticky top-0 z-(--z-header) transition-colors duration-200" style="background-color:var(--header-scroll-bg);border-bottom:1px solid var(--header-scroll-border)">
+    ${TopBar()}
+    ${SubHeader()}
+  </div>
+  ${MegaMenu()}
   <main>
     ${LegalPageLayout(cookiesContent())}
     <div class="max-w-[1200px] mx-auto px-4 sm:px-6 pb-16">
       ${CookieConsentUI()}
     </div>
   </main>
+  <footer>
+    ${FooterLinks()}
+  </footer>
   ${FloatingPanel()}
 `;
 
+initMegaMenu();
 initFlowbite();
-initHelpCenterLangSelector();
 startAlpine();
+initStickyHeaderSearch();
+initMobileDrawer();
+initLanguageSelector();
